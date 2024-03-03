f3ec247fcd5e36e0788f950f88a92793d9169cdb
Mattia Girolimetto <mattia.girolimetto@studio.unibo.it>
2024-03-03 15:39:11 +0100

    chore: cleaned main.go

--- main.go
+++ main.go
@@ -1,20 +1,4 @@
 package main
 
-import "fmt"
-
-func reduce2L(sli []int, i int, f func(int, int) int, v int) int {
-	if i >= len(sli) {
-		return v
-	}
-
-	var z = f(v, sli[i])
-
-	return reduce2L(sli, i+1, f, z)
-}
-
 func main() {
-	s := []int{1, 2, 3}
-
-	fmt.Printf("%d\n", reduce2L(s, 0, func(i1, i2 int) int { fmt.Printf("sum %d + %d\n", i1, i2); return i1 + i2 }, 0))
-
 }
